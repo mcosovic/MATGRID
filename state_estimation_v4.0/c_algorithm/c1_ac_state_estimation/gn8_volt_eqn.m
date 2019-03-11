526dc4b02affb6fabf135dae19b511d86a444753
Mirsad Cosovic <mirsad.cosovic@gmail.com>
2019-03-12 00:45:53 +0100

    Delete gn8_volt_eqn.m

--- state_estimation_v4.0/c_algorithm/c1_ac_state_estimation/gn8_volt_eqn.m
+++
@@ -1,7 +0,0 @@
- function [Fvol] = gn8_volt_eqn(V, T, vma, vmp, ti)
- 
-
-
-%-------------------Voltage Magnitude and Angle Function-------------------                                                           
- Fvol = [V(vma.i); V(vmp.i); T(ti.i)]; 
-%--------------------------------------------------------------------------
